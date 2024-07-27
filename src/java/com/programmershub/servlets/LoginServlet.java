99090d0ced117cc45df7e08a6d5931d76803e3b6
Sunilkarkee <suneelkshetri@gmail.com>
2024-07-27 07:08:55 +0530

    Servlet method is modified to use specific method doPost  to process request

--- src/java/com/programmershub/servlets/LoginServlet.java
+++ src/java/com/programmershub/servlets/LoginServlet.java
@@ -18,8 +18,8 @@ public class LoginServlet extends HttpServlet {
 
     private static final Logger LOGGER = Logger.getLogger(LoginServlet.class.getName());
 
-  
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+  @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
 
@@ -68,17 +68,8 @@ public class LoginServlet extends HttpServlet {
         }
     }
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+  
+    
 
     @Override
     public String getServletInfo() {
